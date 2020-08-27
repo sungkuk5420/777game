44cbe19bbb6810013961179c173fab21d66e3480
sungkuk5420 <sungkuk5420@gmail.com>
2020-08-28 02:27:23 +0900

    터치로 이벤트 변경

--- src/js/vueInit.js
+++ src/js/vueInit.js
@@ -156,25 +156,25 @@ var mainVue = new Vue({
             var touch = null;
             this.currentEvent = Date.now();
 
-            switch (event.type) {
-                case "touchstart":
-                    type = "mousedown";
-                    touch = event.changedTouches[event.changedTouches.length - 1];
-                    break;
-                case "touchmove":
-                    type = "mousemove";
-                    touch = event.changedTouches[event.changedTouches.length - 1];
-                    break;
-                case "touchend":
-                    type = "mouseup";
-                    touch = event.changedTouches[event.changedTouches.length - 1];
-                    break;
-            }
+            // switch (event.type) {
+            //     case "touchstart":
+            //         type = "mousedown";
+            //         touch = event.changedTouches[event.changedTouches.length - 1];
+            //         break;
+            //     case "touchmove":
+            //         type = "mousemove";
+            //         touch = event.changedTouches[event.changedTouches.length - 1];
+            //         break;
+            //     case "touchend":
+            //         type = "mouseup";
+            //         touch = event.changedTouches[event.changedTouches.length - 1];
+            //         break;
+            // }
 
-            newEvt.initMouseEvent(type, true, true, event.target.ownerDocument.defaultView, 0,
-                touch.screenX, touch.screenY, touch.clientX, touch.clientY,
-                event.ctrlKey, event.altKey, event.shiftKey, event.metaKey, 0, null);
-            event.target.dispatchEvent(newEvt);
+            // newEvt.initMouseEvent(type, true, true, event.target.ownerDocument.defaultView, 0,
+            //     touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+            //     event.ctrlKey, event.altKey, event.shiftKey, event.metaKey, 0, null);
+            // event.target.dispatchEvent(newEvt);
 
             func();
         },
