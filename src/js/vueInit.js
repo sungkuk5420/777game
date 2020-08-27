5bb5d7be3758f4e4719b48db85e5d02505394112
sungkuk5420 <sungkuk5420@gmail.com>
2020-08-28 02:07:50 +0900

    터치이벤트 우회 테스트

--- src/js/vueInit.js
+++ src/js/vueInit.js
@@ -144,6 +144,38 @@ var mainVue = new Vue({
     el: '#app',
     data: vueData,
     methods: {
+        onTouch(func) {
+            console.log(event)
+            event.preventDefault();
+            if (event.touches.length > 1 || (event.type == "touchend" && event.touches.length > 0))
+                return;
+
+            var newEvt = document.createEvent("MouseEvents");
+            var type = null;
+            var touch = null;
+
+            switch (event.type) {
+                case "touchstart":
+                    type = "mousedown";
+                    touch = event.changedTouches[0];
+                    break;
+                case "touchmove":
+                    type = "mousemove";
+                    touch = event.changedTouches[0];
+                    break;
+                case "touchend":
+                    type = "mouseup";
+                    touch = event.changedTouches[0];
+                    break;
+            }
+
+            newEvt.initMouseEvent(type, true, true, event.target.ownerDocument.defaultView, 0,
+                touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+                event.ctrlKey, event.altKey, event.shiftKey, event.metaKey, 0, null);
+            event.target.dispatchEvent(newEvt);
+
+            func();
+        },
         buttonClick: function (rowIndex, index) {
 
             var selectedIndex = findWithAttr(vueData.buttons, 'state', 'select');
